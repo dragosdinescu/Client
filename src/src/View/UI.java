36875fc1b4a622c99341815b834ea7f44eb52802
Radu Sorostinean <radu.sorostinean@gmail.com>
2020-05-09 16:43:52 +0100

    Refactoring

--- src/src/View/UI.java
+++ src/src/View/UI.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UI {
@@ -16,9 +17,11 @@ public class UI {
     private JPanel buttonPanel, fieldsPanel, comboxPanel;
     private JButton submit, add, delete, modify, ok;
     private HashMap<String, Contact> contactMap;
-
+    private JComboBox<String> cb;
+    public ArrayList<JTextField> textFields;
     public UI (HashMap<String, Contact> contactMap){
         this.contactMap = contactMap;
+        textFields = new ArrayList<>();
     }
 
     public void startUI() {
@@ -26,15 +29,94 @@ public class UI {
         this.frame.setTitle("Client");
         this.frame.setSize(1000, 1000);
         this.frame.setLocation(200, 200);
-
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setResizable(false);
+        createButtons();
+        createElements();
+        setActionListeners();
+        addElements();
+        setSettings();
+    }
 
+    private void setSettings (){
+        fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.PAGE_AXIS));
+        buttonPanel.setLayout(new FlowLayout());
+        select.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cb.setMaximumSize(cb.getPreferredSize());
+        cb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cb.setVisible(true);
+        ok.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.setSize(500, 300);
+        frame.setLayout(new GridLayout(2, 3));
+        buttonPanel.setLayout(new GridLayout(4, 1));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        frame = new JFrame("Client");
+    private void setActionListeners (){
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Contactul a fost trimis");
+            }
+        });
+
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        modify.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ok.addActionListener(actionEvent -> clearFields());
+        cb.addActionListener( actionEvent -> {
+            String id = (String) cb.getSelectedItem();
+            Contact contact = contactMap.get(id);
+            fillFields(contact);
+        });
+    }
+
+    private void createButtons (){
+        submit = new JButton("Trimite");
+        add = new JButton("Contact nou");
+        delete = new JButton("Sterge");
+        modify = new JButton("Modifica");
+        ok = new JButton("Clear");
+    }
+
+    private void fillFields(Contact contact){
+        this.firstNameField.setText(contact.getFirstName());
+        this.lastNameField.setText(contact.getLastName());
+        this.emailField.setText(contact.getEmail());
+        this.phoneNumberField.setText(contact.getPhoneNumber());
+        this.registrationDateField.setText(String.valueOf(contact.getDate()));
+        this.carrierField.setText(contact.getCarrierEnum());
+    }
+
+    private void clearFields(){
+        for (int i = 0; i <textFields.size() ; i++) {
+            JTextField element = textFields.get(i);
+            element.setText("");
+        }
+    }
+
+    private void createElements(){
         buttonPanel = new JPanel();
         fieldsPanel = new JPanel();
         comboxPanel = new JPanel();
+        frame = new JFrame("Client");
         comboxPanel.setLayout(new BoxLayout(comboxPanel, BoxLayout.Y_AXIS));
         select = new JLabel("Selecteaza");
         firstName = new JLabel("Nume");
@@ -44,43 +126,21 @@ public class UI {
         carrier = new JLabel("Retea");
         registrationDate = new JLabel("Data");
         firstNameField = new JTextField(" ");
+        textFields.add(firstNameField);
         lastNameField = new JTextField(" ");
+        textFields.add(lastNameField);
         emailField = new JTextField(" ");
+        textFields.add(emailField);
         phoneNumberField = new JTextField(" ");
+        textFields.add(phoneNumberField);
         carrierField = new JTextField(" ");
+        textFields.add(carrierField);
         registrationDateField = new JTextField(" ");
-        submit = new JButton("Trimite");
-        submit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Contactul a fost trimis");
-            }
-        });
-        add = new JButton("Contct nou");
-        add.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        textFields.add(registrationDateField);
+        cb = new JComboBox<String>( contactMap.keySet().toArray(new String[0]));
+    }
 
-            }
-        });
-        delete = new JButton("Sterge");
-        delete.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        modify = new JButton("Modifica");
-        modify.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.PAGE_AXIS));
-        buttonPanel.setLayout(new FlowLayout());
-
+    private void addElements(){
         fieldsPanel.add(firstName);
         fieldsPanel.add(firstNameField);
         fieldsPanel.add(lastName);
@@ -94,51 +154,16 @@ public class UI {
         fieldsPanel.add(registrationDate);
         fieldsPanel.add(registrationDateField);
         comboxPanel.add(select);
-        select.setAlignmentX(Component.CENTER_ALIGNMENT);
-        String[] choices = contactMap.keySet().toArray(new String[0]);
-        JComboBox<String> cb = new JComboBox<String>(choices);
-        cb.setMaximumSize(cb.getPreferredSize());
-        cb.setAlignmentX(Component.CENTER_ALIGNMENT);
-        cb.setVisible(true);
-        ok = new JButton("OK");
-        ok.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String id = (String) cb.getSelectedItem();
-                String message = "";
-                Contact contact = contactMap.get(id);
-                message += contact.getFirstName()+"\n"
-                        +contact.getLastName()+"\n"
-                        +contact.getEmail()+"\n"
-                        +contact.getPhoneNumber()+"\n"
-                        +contact.getCarrierEnum()+"\n"
-                        +contact.getDate()+"\n";
-                JOptionPane.showMessageDialog(null,message);
-            }
-        });
-
         comboxPanel.add(cb);
-        ok.setAlignmentX(Component.CENTER_ALIGNMENT);
         comboxPanel.add(ok);
-
         buttonPanel.add(submit);
         buttonPanel.add(add);
         buttonPanel.add(delete);
         buttonPanel.add(modify);
-
-        frame.setSize(500, 300);
-        frame.setLayout(new GridLayout(2, 3));
-        buttonPanel.setLayout(new GridLayout(4, 1));
         frame.add(fieldsPanel);
         frame.add(buttonPanel);
         frame.add(comboxPanel);
-
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
     }
-
 
 }
 
